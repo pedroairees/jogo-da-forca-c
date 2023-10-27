060e0b78353643f23ef1cd497abb0437cd771b0f
pedroaiires <pedrocoelho2209@gmail.com>
2023-10-27 11:25:07 -0300

    subindo

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+void desenhaforca();
+void adicionaPalavra();
+void escolhepalavra();
+int acertou();
+int chutesErrados();
+int enforcou();
+int jachutou(char letra);
